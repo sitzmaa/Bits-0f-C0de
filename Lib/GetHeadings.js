80a436076dfee32c792290e57c3ffbfa9bec2a1f
sitzmaa <103063243+sitzmaa@users.noreply.github.com>
2024-05-31 20:29:30 -0700

    blog

--- Lib/GetHeadings.js
+++ Lib/GetHeadings.js
@@ -1,30 +1,41 @@
-export async function getHeadings(source) {
-  // Get each line individually, and filter out anything that
-  // isn't a heading.
-  const headingLines = source.split("\n").filter((line) => {
-    return line.match(/^###*\s/);
-  });
+import { AiOutlineArrowRight } from "react-icons/ai";
+import Link from "next/link";
 
-  // Transform the string '## Some text' into an object
-  // with the shape '{ text: 'Some text', level: 2 }'
+function BlogHeader({ data, content, readTime }) {
+  return (
+    <Link
+      href={`/blogs/${String(data.Title.split(" ").join("-").toLowerCase())}`}
+    >
+      <div className="cursor-pointer group px-6 py-6 md:px-10 flex flex-col items-start rounded transform h-full w-full">
+        <span className="inline-block py-1 px-2 rounded bg-gray-100 dark:bg-indigo-900 text-indigo-700 dark:text-gray-200 text-sm font-medium tracking-widest">
+          {data.Tags.split(" ")[0]}
+        </span>
+        <h2 className="sm:text-2xl text-xl title-font font-semibold text-gray-700 mt-4 mb-4 dark:text-gray-100 group-hover:text-indigo-800 dark:group-hover:text-indigo-400">
+          {data.Title}
+        </h2>
+        <p className="leading-relaxed mb-5 text-gray-800 dark:text-gray-200">
+          {data.Abstract}..
+        </p>
 
-  let uid = 1000;
-  return headingLines.map((raw) => {
-    const text = raw
-      .replace(/^###*\s/, "")
-      .replace(/ *\{[^)]*\} */g, "")
-      .trim();
-    // I only care about h2 and h3.
-    // If I wanted more levels, I'd need to count the
-    // number of #s.
-    const id = text
-      .replace(/[&\/\\#,+()$~%'":*?<>{}]/g, "")
-      .trim()
-      .split(" ")
-      .join("-");
+        <div className="flex items-center flex-wrap pb-2 border-b-2 border-gray-300 mt-auto w-full justify-between dark:border-indigo-400">
+          <a className="text-indigo-700 inline-flex items-center dark:text-indigo-200 group-hover:text-indigo-800 dark:group-hover:text-indigo-400">
+            Learn More{" "}
+            <span className="pl-1">
+              <AiOutlineArrowRight />
+            </span>
+          </a>
 
-    const level = raw.slice(0, 3) === "###" ? 3 : 2;
-    uid++;
-    return { text, level, id, uid };
-  });
+          <a className="inline-flex items-center">
+            <span className="flex-grow flex flex-col pl-4">
+              <span className="text-gray-900 text-xs tracking-widest mt-0.5 dark:text-indigo-300">
+                {readTime}
+              </span>
+            </span>
+          </a>
+        </div>
+      </div>
+    </Link>
+  );
 }
+
+export default BlogHeader;
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import { getAllBlogPosts, getAllTopics } from "../Lib/Data";
 
 export const getStaticProps = async () => {
   const allBlogs = await getAllBlogPosts();
-  const allTopics = await getAllTopics(); // Wait for the promise to resolve
+  const allTopics = await getAllTopics();
   return {
     props: {
       blogs: allBlogs,
@@ -26,7 +26,6 @@ export default function Home({ blogs, topics }) {
           name="description"
           content="Project Blog space for Alexander Sitzman"
         />
-
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://blogs.alexsitzman.com/" />
         <meta property="og:title" content="Alexander's Blog" />
@@ -38,7 +37,6 @@ export default function Home({ blogs, topics }) {
           property="og:image"
           content="https://raw.githubusercontent.com/soumyajit4419/Bits-0f-C0de/main/Extra/sc.png"
         />
-
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://blogs.alexsitzman.com/" />
         <meta property="twitter:title" content="Alexander's Blog" />
@@ -55,24 +53,23 @@ export default function Home({ blogs, topics }) {
       <div className="min-h-screen relative bg-white dark:bg-gray-900">
         <Navbar topics={topics} />
         <Header />
-
         <div className="px-0.5 md:px-7 pb-14 pt-6">
-          <div className="flex flex-wrap">
+          <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             {blogs &&
               blogs.map(
                 (blog) =>
                   blog.data.isPublished && (
-                    <BlogHeader
-                      key={blog.data.Id}
-                      data={blog.data}
-                      content={blog.content}
-                      readTime={blog.readTime.text}
-                    />
+                    <div key={blog.data.Id} className="aspect-w-1 aspect-h-1">
+                      <BlogHeader
+                        data={blog.data}
+                        content={blog.content}
+                        readTime={blog.readTime.text}
+                      />
+                    </div>
                   )
               )}
           </div>
         </div>
-
         <Footer />
       </div>
     </>
